762fd536009c0b09d8ae8b933c473a7c67421251
Sulaiman Nadeem <sunnad99@gmail.com>
2024-08-04 15:46:00 +0500

    feat: Added a simple form for creating customer accounts

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const FormInput = (props) => {
+  const name = props.name;
+  const value = props.value;
+  const type = props.type;
+  const eventHandler = props.eventHandler;
+  const additionalStyles = props.style;
+
+  let style = {
+    margin: "10px 10px",
+  };
+
+  if (additionalStyles) {
+    style = { ...style, ...additionalStyles };
+  }
+
+  return (
+    <div style={style}>
+      <p>{name}</p>
+      <input
+        value={value}
+        onChange={eventHandler}
+        type={type ? type : "text"}
+      />
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleFirstNameChange = (event) => {
+    console.log("First name is:", event.target.value);
+
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    console.log("Last name is:", event.target.value);
+
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    console.log("Email is:", event.target.value);
+
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    console.log("Password is:", event.target.value);
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    console.log("Confirm Password is:", event.target.value);
+
+    setConfirmPassword(event.target.value);
+  };
+
+  const addCustomer = (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      // TODO: Add a better way to display errors
+      alert("Passwords do not match");
+    }
+
+    console.log("button click", event.target);
+  };
+
+  const nameStyle = {
+    display: "flex",
+  };
+
+  const containerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={containerStyle}>
+        <form onSubmit={addCustomer}>
+          <div style={nameStyle}>
+            <FormInput
+              name="First Name"
+              value={firstName}
+              eventHandler={handleFirstNameChange}
+            />
+
+            <FormInput
+              name="Last Name"
+              value={lastName}
+              eventHandler={handleLastNameChange}
+            />
+          </div>
+
+          <FormInput
+            name="Email"
+            type="email"
+            value={email}
+            eventHandler={handleEmailChange}
+            style={containerStyle}
+          />
+
+          <FormInput
+            name="Password"
+            type="password"
+            value={password}
+            eventHandler={handlePasswordChange}
+            style={containerStyle}
+          />
+
+          <FormInput
+            name="Confirm Password"
+            type="password"
+            value={confirmPassword}
+            eventHandler={handleConfirmPasswordChange}
+            style={containerStyle}
+          />
+
+          <div style={containerStyle}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
