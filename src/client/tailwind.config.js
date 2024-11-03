0051ae9f7dc21ea6366aba2f2c168ee36c8df6f5
Sulaiman Nadeem <sunnad99@gmail.com>
2024-11-03 17:54:24 +0500

    refactor: Returned border radius back to normal

--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -4,11 +4,7 @@ export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
+      borderRadius: {},
       colors: {},
     },
   },
